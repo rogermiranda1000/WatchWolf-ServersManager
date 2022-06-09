e9b2e0362410a321db8bc7f58add01ec8002d77c
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-06-09 19:07:49 +0200

    Fix: server base read-only

--- ServersManager.sh
+++ ServersManager.sh
@@ -5,5 +5,5 @@ cpu="4"
 
 java="8"
 mc_version="1.12.2"
-cmd="cd /server ; java -Xmx${memory_limit^^} -jar spigot-$mc_version.jar nogui"
-sudo docker run -i --rm --entrypoint /bin/sh --name test -p 8080:80 --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/server":/server "openjdk:$java" <<< "$cmd"
+cmd="cp -r /server/* ~/ ; cd ~/ ; java -Xmx${memory_limit^^} -jar spigot-$mc_version.jar nogui"
+sudo docker run -i --rm --entrypoint /bin/sh --name test -p 8080:80 --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/server":/server:ro "openjdk:$java" <<< "$cmd"
