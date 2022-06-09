742ca0289bfa09e5c687ed02283a49882530934c
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-06-09 19:15:57 +0200

    Feature: specify port

--- ServersManager.sh
+++ ServersManager.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-memory_limit="7g" # 6GB of RAM for the MC server, 1GB for Ubuntu
+# Hard limits
+memory_limit="4g"
 cpu="4"
 
-java="8"
+# MC configuration
 mc_version="1.12.2"
-cmd="cp -r /server/* ~/ ; cd ~/ ; java -Xmx${memory_limit^^} -jar spigot-$mc_version.jar nogui"
-sudo docker run -i --rm --entrypoint /bin/sh --name test -p 8080:80 --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/server":/server:ro "openjdk:$java" <<< "$cmd"
+port="8001"
+java="8" # TODO change java version with mc_version
+cmd="cp -r /server/* ~/ ; cd ~/ ; java -Xmx${memory_limit^^} -jar spigot-$mc_version.jar nogui" # copy server base and run it
+
+sudo docker run -i --rm --name "Minecraft_$mc_version" -p "$port:25565" --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/server":/server:ro "openjdk:$java" <<< "$cmd"
