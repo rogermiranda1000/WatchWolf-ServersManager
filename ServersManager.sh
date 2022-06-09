2d046ff673cfe02e8b3e80fefe3383f57e60cbd3
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-06-09 18:51:12 +0200

    Feature: start server

--- ServersManager.sh
+++ ServersManager.sh
@@ -4,5 +4,6 @@ memory_limit="7g" # 6GB of RAM for the MC server, 1GB for Ubuntu
 cpu="4"
 
 java="8"
-cmd="apt-get update && apt-get -y install -qq openjdk-$java-jdk"
-sudo docker run -i --rm --entrypoint /bin/sh --name test -p 8080:80 --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/test":/data ubuntu <<< "$cmd"
+mc_version="1.12.2"
+cmd="cd /server ; java -Xmx${memory_limit^^} -jar spigot-$mc_version.jar nogui"
+sudo docker run -i --rm --entrypoint /bin/sh --name test -p 8080:80 --memory="$memory_limit" --cpus="$cpu" -v "$(pwd)/server":/server "openjdk:$java" <<< "$cmd"
