8438bf8909e4afffac67fb10cdd8c765c0f32529
roger.miranda <roger.miranda@students.salle.url.edu>
2023-03-08 16:58:00 +0100

    Feature: build

--- PaperBuilder.sh
+++ PaperBuilder.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-function getAllVersions {
-	# 'https://hub.spigotmc.org/versions' contains all the version files
-	curl -s https://api.papermc.io/v2/projects/paper/ | jq -c '.versions | .[]' | grep -o -P '(?<=")1\.\d+(\.\d+)?(?=")' | sort --reverse --version-sort --field-separator=.
-}
+source ./SpigotBuilder.sh
 
-# @param server_version
-function get_java_version {
-	case `echo "$1" | grep -o -P '^\d+\.\d+'` in # get the first 2 numbers
-		"1.19" | "1.18" )
-			return 17
-			;;
-		"1.17" )
-			return 16
-			;;
-		* ) # previous to 1.17
-			return 8
-			;;
-	esac
+function getAllPaperVersions {
+	# 'https://api.papermc.io/v2/projects/paper/' contains a JSON with all the MC versions
+	curl -s https://api.papermc.io/v2/projects/paper/ | jq -c '.versions | .[]' | grep -o -P '(?<=")1\.\d+(\.\d+)?(?=")' | sort --reverse --version-sort --field-separator=.
 }
 
 # @param absolute_copy_path
 # @param version (from getAllVersions)
-function buildVersion {
+function buildPaperVersion {
 	mc_version="$2"
 	get_java_version "$mc_version"
 	java_version="$?"
 	
+	# 'https://api.papermc.io/v2/projects/paper/versions/<mc_version>/builds/' contains all the builds from that version
+	# It returns a JSON with the last element being the most recent. We'll want the build number and the file name to get the download link.
 	base_path="https://api.papermc.io/v2/projects/paper/versions/$mc_version/builds/"
 	download_path=`curl -s "$base_path" | jq --raw-output '.builds[-1] | .build,.downloads.application.name' | awk -v base="$base_path" '{build=$0; getline; print base build "/downloads/" $0;}'`
 	
-	cmd="mkdir BuildTools; cd BuildTools; wget $download_path && java  && cp spigot-$mc_version.jar /Versions/$mc_version.jar"
+	pre_cmd=":" # NOP; in Java 8 you need to use 'apt-get', and wget is already installed
+	if [ $java_version -ne 8 ]; then
+		# TODO use an image with wget already installed
+		pre_cmd="microdnf install wget" # in Java 16-17 wget is not installed
+	fi
+	
+	# Paper gets built on the first server execution; we'll start the server, and then stop it and copy the compiled server
+	server_file="paper-$mc_version.jar"
+	cmd="$pre_cmd; mkdir BuildTools; cd BuildTools; wget -O $server_file $download_path && echo 'eula=true' > eula.txt && echo 'stop' | java -jar $server_file nogui && cp $server_file /Versions/$mc_version.jar"
 	sudo docker run -i --rm --detach --name "Paper_build_$mc_version" -v "$1":/Versions "openjdk:$java_version" /bin/bash -c "$cmd"
 }
