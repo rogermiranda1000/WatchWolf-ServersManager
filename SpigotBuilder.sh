63f348469120fb8c8ad8237daa9f895343e5b7c8
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-03-08 15:27:43 +0100

    Auto get Spigot versions

--- SpigotBuilder.sh
+++ SpigotBuilder.sh
@@ -1,42 +1,8 @@
 #!/bin/bash
 
-# From https://www.spigotmc.org/wiki/buildtools
 function getAllVersions {
-	echo "1.19"
-	echo "1.18.2"
-	echo "1.18.1"
-	echo "1.18"
-	echo "1.17.1"
-	echo "1.17"
-	echo "1.16.5"
-	echo "1.16.4"
-	echo "1.16.3"
-	echo "1.16.2"
-	echo "1.16.1"
-	echo "1.15.2"
-	echo "1.15.1"
-	echo "1.15"
-	echo "1.14.4"
-	echo "1.14.3"
-	echo "1.14.2"
-	echo "1.14.1"
-	echo "1.14"
-	echo "1.13.2"
-	echo "1.13.1"
-	echo "1.13"
-	echo "1.12.2"
-	echo "1.12.1"
-	echo "1.12"
-	echo "1.11.2"
-	echo "1.11.1"
-	echo "1.11"
-	echo "1.10.2"
-	echo "1.9.4"
-	echo "1.9.2"
-	echo "1.9"
-	echo "1.8.8"
-	echo "1.8.3"
-	echo "1.8"
+	# 'https://hub.spigotmc.org/versions' contains all the version files
+	curl https://hub.spigotmc.org/versions/ | grep -o -P '1\.\d+(\.\d+)?(?=\.json)' | sort --reverse --version-sort --field-separator=. | uniq -d
 }
 
 # @param server_version
