5e09db8b56f8d4e118e2ad9398c75cf44a15cc65
rogermiranda1000 <rogermirandaperez@hotmail.com>
2024-03-06 22:17:45 +0100

    CI: launch integration tests

--- ci/debug/tests.sh
+++ ci/debug/tests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # default variables
-unit=1
-system=0
+unit=0
+integration=0
 
 # parse params
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --unit) unit=1 ;;
-        --system) system=1 ;;
+        --integration) integration=1 ;;
         
         *) echo "[e] Unknown parameter passed: $1" >&2 ; exit 1 ;;
     esac
     shift
 done
 
-if [ $system -eq 0 ] && [ $unit -eq 0 ]; then
+if [ $integration -eq 0 ] && [ $unit -eq 0 ]; then
     echo "[e] You must specify at least one type of test to run!"
     exit 1
 fi
@@ -28,7 +28,7 @@ local_maven_repos_path="$HOME/.m2"
 # run the tests
 if [ $unit -eq 1 ]; then
     # run unit tests
-    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 maven:3.8.3-openjdk-17 mvn test site -DskipTests=false --file '/compile'
+    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 maven:3.8.3-openjdk-17 mvn test -DskipTests=false -DskipUTs=false -DskipITs=true --file '/compile'
     result=$?
 
     # Convert xml reports into html
@@ -40,12 +40,17 @@ if [ $unit -eq 1 ]; then
     fi
 fi 
 
-if [ $system -eq 1 ]; then
-    # run system tests
-    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock maven:3.8.3-openjdk-17 mvn test -DskipITs=false --file '/compile'
+if [ $integration -eq 1 ]; then
+    # run integration tests
+    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock maven:3.8.3-openjdk-17 mvn failsafe:integration-test --file '/compile'
+    result=$?
 
-    if [ $? -ne 0 ]; then
-        echo "[e] System tests failed"
+    # Convert xml reports into html
+    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 maven:3.8.3-openjdk-17 mvn surefire-report:failsafe-report-only --file '/compile'
+    docker run -it --rm -v "$base_path":/compile -v "$local_maven_repos_path":/root/.m2 maven:3.8.3-openjdk-17 mvn site -DgenerateReports=false --file '/compile'
+
+    if [ $result -ne 0 ]; then
+        echo "[e] Integration tests failed"
     fi
 fi
 
--- ci/debug/validator.sh
+++ ci/debug/validator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+error=0
+
 script_path=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 base_path=$(dirname $(dirname "$script_path"))
 unit_tests_path="$base_path/src/test/java"
@@ -11,7 +13,16 @@ if [ ! -z "$warning_files" ]; then
     echo "[w] Some files are not following the unit test naming convention. Any unit test that don't end with 'Should' won't run."
     echo "[v] Files that don't follow the convention:"
     echo "$warning_files"
-    exit 1
+    error=1
+fi
+
+# are unit tests following other naming standard?
+warning_files=`find "$unit_tests_path" -type f -name 'IT*.java'` # every java file that start with "IT"
+if [ ! -z "$warning_files" ]; then
+    echo "[w] Some files are not following the unit test naming convention. Any unit test that start with 'IT' won't run."
+    echo "[v] Files that don't follow the convention:"
+    echo "$warning_files"
+    error=1
 fi
 
 # are integration tests following naming standard?
@@ -20,7 +31,10 @@ if [ ! -z "$warning_files" ]; then
     echo "[w] Some files are not following the system test naming convention. Any system test that don't start with 'IT' won't run."
     echo "[v] Files that don't follow the convention:"
     echo "$warning_files"
-    exit 1
+    error=1
 fi
 
+if [ $error -ne 0 ]; then
+    exit $error
+fi
 echo "[i] All done"
